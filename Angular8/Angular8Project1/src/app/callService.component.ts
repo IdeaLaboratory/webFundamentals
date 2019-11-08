91bfaa2f9ee05e5d661ca357feb5ed31e16c2489
Mondal <adi.mondal@siemens.com>
2019-11-08 12:42:19 +0530

    Printing property of callService component

--- Angular8/Angular8Project1/src/app/callService.component.ts
+++ Angular8/Angular8Project1/src/app/callService.component.ts
@@ -4,12 +4,19 @@ import { RESTAPIService } from './restapi.service';
 //@injectable not require. I am allowing DI in this class.
 @Component({
     selector: 'service',
-    template: "<h1>HTTP service</h1>",
+    template: `<h1>HTTP service</h1>
+    <table border=2>
+    <tr *ngFor="let e of post">
+    <td>{{e.userID}}</td>
+    <td>{{e.id}}</td>
+    </tr>
+    </table>`,
     providers: [RESTAPIService]
 })
 export class CallServiceComponent {
+    post;
     constructor(private ser: RESTAPIService) /*DI*/ {
-        this.ser.getData().subscribe(x => console.log(x)
+        this.ser.getData().subscribe(x => this.post = x
         )
     }
 }
